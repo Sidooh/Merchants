cb1122de65dc2773bc59e05fee435d2a2e9fe72e
DrH <jmnabangi@gmail.com>
2023-10-02 00:27:56 +0300

    fix earnings index
    Signed-off-by: DrH <jmnabangi@gmail.com>

--- pkg/entities/earning.go
+++ pkg/entities/earning.go
@@ -6,11 +6,11 @@ type Earning struct {
 	Amount float32 `json:"amount" gorm:"not null;type:decimal(7,2);"`
 	Type   string  `json:"type" gorm:"size:16;"` //SELF / INVITE / SYSTEM
 
-	TransactionId uint `json:"transaction_id" gorm:"not null;uniqueIndex"`
+	TransactionId uint `json:"transaction_id" gorm:"not null;uniqueIndex:idx_earnings"`
 
 	Transaction Transaction `json:"-"`
 
-	MerchantId uint `json:"merchantId" gorm:"uniqueIndex"`
+	MerchantId uint `json:"merchantId" gorm:"uniqueIndex:idx_earning"`
 
 	Merchant Merchant `json:"-"`
 
