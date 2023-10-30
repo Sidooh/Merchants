8e545bfd2c71b6d8dfe148645711720c72372004
DrH <jmnabangi@gmail.com>
2023-10-30 10:09:34 +0300

    adds status to tx response
    Signed-off-by: DrH <jmnabangi@gmail.com>

--- api/presenter/transaction.go
+++ api/presenter/transaction.go
@@ -4,6 +4,7 @@ type Transaction struct {
 	Id          uint    `json:"id"`
 	Description string  `json:"description"`
 	Destination string  `json:"destination"`
+	Status      string  `json:"status"`
 	Amount      float32 `json:"amount"`
 	MerchantId  uint    `json:"merchant"`
 	Product     string  `json:"product"`
